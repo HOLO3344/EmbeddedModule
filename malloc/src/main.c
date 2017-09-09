2c1153b7fe374a6f8f75c78450bd9bdc2600558f
holo <holo3344@163.com>
2017-09-09 22:19:52 +0800

    add makefile for project

---
+++ malloc/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	printf("BUILED TIME: %s, FILE NAME: %s\r\n", __TIME__, __FILE__);
+	return 0;
+}
---
+++ project/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("BUILD AT: %s\n", __TIME__);
+    return 0;
+}
